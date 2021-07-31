52f919f5891a3e9bfe3d287c3fbd5ecdad5b5680
GENG YUE <86158319+creatoryue@users.noreply.github.com>
2021-07-31 13:39:09 +0800

    Update app5.py

--- app5.py
+++ app5.py
@@ -1,25 +1,7 @@
-import streamlit as st
-import pandas as pd
-
 import streamlit.components.v1 as components
 
-_custom_dataframe = components.declare_component(
-    "custom_dataframe", url="http://localhost:3001",
-)
+# Declare the component:
+my_component = components.declare_component("my_component", path="frontend/build")
 
-
-def custom_dataframe(data, key=None):
-    return _custom_dataframe(data=data, key=key, default=pd.DataFrame())
-
-
-raw_data = {
-    "First Name": ["Jason", "Molly", "Tina", "Jake", "Amy"],
-    "Last Name": ["Miller", "Jacobson", "Ali", "Milner", "Smith"],
-    "Age": [42, 52, 36, 24, 73],
-}
-
-df = pd.DataFrame(raw_data, columns=["First Name", "Last Name", "Age"])
-returned_df = custom_dataframe(df)
-
-if not returned_df.empty:
-    st.table(returned_df)
+# Use it:
+my_component(greeting="Hello", name="World")
