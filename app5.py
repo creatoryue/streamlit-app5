c5ca3d57c5743c4fb9dbd6c58fb18821c25166d5
GENG YUE <86158319+creatoryue@users.noreply.github.com>
2021-07-31 14:33:35 +0800

    Update app5.py

--- app5.py
+++ app5.py
@@ -1,7 +1,1 @@
-import streamlit.components.v1 as components
-
-# Declare the component:
-my_component = components.declare_component("my_component", path="frontend/build")
-
-# Use it:
-my_component(greeting="Hello", name="World")
+import streamlit
