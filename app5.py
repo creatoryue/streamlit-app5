fffb74935bec3ba2334faba393792c11c572233b
GENG YUE <86158319+creatoryue@users.noreply.github.com>
2021-07-31 14:54:46 +0800

    Update app5.py

--- app5.py
+++ app5.py
@@ -6,32 +6,35 @@ from streamlit_bokeh_events import streamlit_bokeh_events
 stt_button = Button(label="Speak", width=100)
 
 stt_button.js_on_event("button_click", CustomJS(code="""
-    var recognition = new webkitSpeechRecognition();
-    recognition.continuous = true;
-    recognition.interimResults = true;
- 
-    recognition.onresult = function (e) {
-        var value = "";
-        for (var i = e.resultIndex; i < e.results.length; ++i) {
-            if (e.results[i].isFinal) {
-                value += e.results[i][0].transcript;
-            }
-        }
-        if ( value != "") {
-            document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
-        }
+    const context = new AudioContext()
+    
+    setupContext()
+    
+    asyn function setupContext() {
+        const microphone = await getMicrophone()
+        const source = context.createMediaStreamSource(microphone)
+        source.connect(context.destination)
     }
-    recognition.start();
+    
+    function getMicrophone() {
+        return navigator.mediaDevices.getUserMedia({
+            audio:{
+                echoCancellation: flase,
+                autoGainControl: false,
+                noiseSuppression: false,
+                latency: 0
+            }
+        })
+    }
+    
     """))
 
 result = streamlit_bokeh_events(
     stt_button,
-    events="GET_TEXT",
+    events="setupContext",
     key="listen",
     refresh_on_update=False,
     override_height=75,
     debounce_time=0)
 
-if result:
-    if "GET_TEXT" in result:
-        st.write(result.get("GET_TEXT"))
+
