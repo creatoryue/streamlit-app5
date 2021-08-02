515bd45a45edd3e4db9d749acbba8e9b26431a57
GENG YUE <86158319+creatoryue@users.noreply.github.com>
2021-08-02 16:42:56 +0800

    Update app5.py

--- app5.py
+++ app5.py
@@ -12,3 +12,37 @@ from pathlib import Path
 HERE = Path(__file__).parent
 
 logger = logging.getLogger(__name__)
+
+
+def app_sendonly_audio():
+    """A sample to use WebRTC in sendonly mode to transfer audio frames
+    from the browser to the server and visualize them with matplotlib
+    and `st.pyplot`."""
+    webrtc_ctx = webrtc_streamer(
+        key="sendonly-audio",
+        mode=WebRtcMode.SENDONLY,
+        audio_receiver_size=256,
+        client_settings=WEBRTC_CLIENT_SETTINGS,
+    )
+
+
+if _name_ == '_main_':
+    import os
+
+    DEBUG = os.environ.get("DEBUG", "false").lower() not in ["false", "no", "0"]
+
+    logging.basicConfig(
+        format="[%(asctime)s] %(levelname)7s from %(name)s in %(pathname)s:%(lineno)d: "
+        "%(message)s",
+        force=True,
+    )
+
+    logger.setLevel(level=logging.DEBUG if DEBUG else logging.INFO)
+    
+    st_webrtc_logger = logging.getLogger("streamlit_webrtc")
+    st_webrtc_logger.setLevel(logging.DEBUG)
+
+    fsevents_logger = logging.getLogger("fsevents")
+    fsevents_logger.setLevel(logging.WARNING)
+    
+    app_sendonly_audio()
