ce1428e4b8554c42b50f551a8a80a4fe47d36463
GENG YUE <86158319+creatoryue@users.noreply.github.com>
2021-08-02 17:36:12 +0800

    Update app5.py

--- app5.py
+++ app5.py
@@ -11,7 +11,6 @@ from streamlit_webrtc import (
 )
 
 
-
 HERE = Path(__file__).parent
 logger = logging.getLogger(__name__)
 
@@ -34,6 +33,38 @@ def app_sendonly_audio():
         audio_receiver_size=256,
         client_settings=WEBRTC_CLIENT_SETTINGS,
     )
+    
+    sound_window_len = 5000  # 5s
+    sound_window_buffer = None
+    while True:
+        if webrtc_ctx.audio_receiver:
+            try:
+                audio_frames = webrtc_ctx.audio_receiver.get_frames(timeout=1)
+            except queue.Empty:
+                logger.warning("Queue is empty. Abort.")
+                break
+
+            sound_chunk = pydub.AudioSegment.empty()
+            for audio_frame in audio_frames:
+                sound = pydub.AudioSegment(
+                    data=audio_frame.to_ndarray().tobytes(),
+                    sample_width=audio_frame.format.bytes,
+                    frame_rate=audio_frame.sample_rate,
+                    channels=len(audio_frame.layout.channels),
+                )
+                sound_chunk += sound
+
+            if len(sound_chunk) > 0:
+                if sound_window_buffer is None:
+                    sound_window_buffer = pydub.AudioSegment.silent(
+                        duration=sound_window_len
+                    )
+
+                sound_window_buffer += sound_chunk
+                if len(sound_window_buffer) > sound_window_len:
+                    sound_window_buffer = sound_window_buffer[-sound_window_len:]
+    
+    
 def main():
     
     st.header("# Classificaion for lung condition demo.")
