59fabc8d2496126f3ffd6278448577cd3a89c2e0
GENG YUE <86158319+creatoryue@users.noreply.github.com>
2021-08-03 11:02:36 +0800

    Update app5.py

--- app5.py
+++ app5.py
@@ -63,17 +63,19 @@ def app_sendonly_audio():
                 sound_window_buffer += sound_chunk
                 if len(sound_window_buffer) > sound_window_len:
                     sound_window_buffer = sound_window_buffer[-sound_window_len:]
+            
+            if sound_window_buffer:
+                sound_window_buffer = sound_window_buffer.set_channels(1)
+                sample = np.array(sound_window_buffer.get_array_of_samples())
+
     
-    sound_window_buffer = sound_window_buffer.set_channels(1)
-    sample = np.array(sound_window_buffer.get_array_of_samples())
-    
-    return sample;
+    #return sample;
 
 def main():
     
     st.header("# Classificaion for lung condition demo.")
     "### Recording"
-    sdata = app_sendonly_audio()
+    app_sendonly_audio()
     
 
     
