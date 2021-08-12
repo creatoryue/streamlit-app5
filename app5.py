db07bb261220fc163bab2437273c318a41c2db1f
GENG YUE <86158319+creatoryue@users.noreply.github.com>
2021-08-12 13:46:50 +0800

    Update app5.py

--- app5.py
+++ app5.py
@@ -39,40 +39,7 @@ def app_sendonly_audio():
         client_settings=WEBRTC_CLIENT_SETTINGS,
     )
     
-    sound_window_len = 5*1000  # 5s
-    sound_window_buffer = None
-    while True:
-        if webrtc_ctx.audio_receiver:
-            try:
-                audio_frames = webrtc_ctx.audio_receiver.get_frames(timeout=1)
-            except queue.Empty:
-                logger.warning("Queue is empty. Abort.")
-                break
 
-            sound_chunk = pydub.AudioSegment.empty()
-            for audio_frame in audio_frames:
-                sound = pydub.AudioSegment(
-                    data=audio_frame.to_ndarray().tobytes(),
-                    sample_width=audio_frame.format.bytes,
-                    frame_rate=audio_frame.sample_rate,
-                    channels=len(audio_frame.layout.channels),
-                )
-                sound_chunk += sound
-
-            if len(sound_chunk) > 0:
-                if sound_window_buffer is None:
-                    sound_window_buffer = pydub.AudioSegment.silent(
-                        duration=sound_window_len
-                    )
-
-                sound_window_buffer += sound_chunk
-                if len(sound_window_buffer) > sound_window_len:
-                    sound_window_buffer = sound_window_buffer[-sound_window_len:]
-
-            if sound_window_buffer:
-                sound_window_buffer = sound_window_buffer.set_channels(1)
-                sample = np.array(sound_window_buffer.get_array_of_samples())
-                return sample
 
     
 
@@ -80,7 +47,7 @@ def main():
     
     st.header("# Classificaion for lung condition demo.")
     "### Recording"
-    sdata = app_sendonly_audio()
+    # sdata = app_sendonly_audio()
     
     state_playButton = st.button("Click to show")
     if state_playButton:
