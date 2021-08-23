3e58144e1f06a270c234cd628ce7a329f1dfe0b7
GENG YUE <86158319+creatoryue@users.noreply.github.com>
2021-08-23 15:51:57 +0800

    Update app555.py

--- app555.py
+++ app555.py
@@ -3,4 +3,4 @@ import streamlit.components.v1 as components
 import base64
 
 st.header("test html import")
-tmlFile = open("test.html", 'r', encoding='utf-8')
+tmlFile = open("index.html", 'r', encoding='utf-8')
