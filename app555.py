f0f5004e4ddf3c87017111f8ebd904b2a4cb3cea
GENG YUE <86158319+creatoryue@users.noreply.github.com>
2021-08-23 18:07:24 +0800

    Update app555.py

--- app555.py
+++ app555.py
@@ -2,8 +2,12 @@ import streamlit as st
 import streamlit.components.v1 as components
 import base64
 
+from flask import Flask
+from flask import render_template
+
 st.header("test html import")
-HtmlFile = open("index.html", 'r', encoding='utf-8')
-source_code = HtmlFile.read() 
-print(source_code)
-components.html(source_code, height= 1600, width=1600)
+#HtmlFile = open("index.html", 'r', encoding='utf-8')
+#source_code = HtmlFile.read() 
+#print(source_code)
+#components.html(source_code, height= 1600, width=1600)
+
