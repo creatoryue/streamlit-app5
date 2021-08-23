ed2335c1b7636877ad3d2a9a4c024dc7926424c8
GENG YUE <86158319+creatoryue@users.noreply.github.com>
2021-08-23 19:18:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ st.header("test html import")
 HtmlFile = open("index.html", 'r', encoding='utf-8')
 source_code = HtmlFile.read() 
 print(source_code)
-components.html(source_code, height= 1600, width=1600)
+# components.html(source_code, height= 1600, width=1600)
 
 components.iframe("https://docs.streamlit.io/en/latest")
 
